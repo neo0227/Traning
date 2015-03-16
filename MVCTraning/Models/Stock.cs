d860a74d5974af5466e5985b30dfb33b01fb65ae
nick <nick.lin@macrowell.com.tw>
2015-03-16 16:05:47 +0800

    test Model Code First

--- MVCTraning/Models/Stock.cs
+++ MVCTraning/Models/Stock.cs
@@ -2,15 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MVCTraning.StockTicker
 {
 	public class Stock
 	{
 		private decimal _price;
-
+		[Key]
+		[Required] 
+		[DatabaseGenerated(DatabaseGeneratedOption.None)]
+		public int StockID { set; get; }
+		[MaxLength(10)]
+		[Required] 
 		public string Symbol { get; set; }
-
+		[Required] 
 		public decimal Price
 		{
 			get
@@ -32,9 +41,9 @@ namespace MVCTraning.StockTicker
 				}
 			}
 		}
-
+		[NotMapped] 
 		public decimal DayOpen { get; private set; }
-
+		[NotMapped] 
 		public decimal Change
 		{
 			get
@@ -42,7 +51,7 @@ namespace MVCTraning.StockTicker
 				return Price - DayOpen;
 			}
 		}
-
+		[NotMapped] 
 		public double PercentChange
 		{
 			get
@@ -51,4 +60,11 @@ namespace MVCTraning.StockTicker
 			}
 		}
 	}
+
+	public class StockDBContext : DbContext
+	{
+		public StockDBContext() : base("StockDBContext") { }
+		public DbSet<Stock> Stocks { get; set; }
+	}
+
 }
